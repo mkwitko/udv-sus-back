2244b3ce9d971098f38f3fd55ef5b4b4b66955cb
mkwitko <oficialkwitko@gmail.com>
2025-04-08 10:24:26 -0300

    fix: middleware

--- src/api/v1/controllers/faq/find-faq.ts
+++ src/api/v1/controllers/faq/find-faq.ts
@@ -1,30 +1,26 @@
 import type { FastifyInstance } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
-import { authenticationMiddleware } from "@/middlewares/authentication-middleware";
 import { faqResponse } from "./create-faq";
 import { findFaqService } from "../../services/faq/find-faq-service";
 
 export async function findFaqs(app: FastifyInstance) {
-  app
-    .withTypeProvider<ZodTypeProvider>()
-    .register(authenticationMiddleware)
-    .get(
-      "/faq/find/all",
-      {
-        schema: {
-          tags: ["Faq"],
-          summary: "Find All Faqs",
-          description: "Find all Faqs",
-          operationId: "findAllFaqs",
-          response: {
-            201: z.array(faqResponse),
-          },
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/faq/find/all",
+    {
+      schema: {
+        tags: ["Faq"],
+        summary: "Find All Faqs",
+        description: "Find all Faqs",
+        operationId: "findAllFaqs",
+        response: {
+          201: z.array(faqResponse),
         },
       },
-      async (_, response) => {
-        const { faq } = await findFaqService();
-        return response.status(201).send(faq);
-      }
-    );
+    },
+    async (_, response) => {
+      const { faq } = await findFaqService();
+      return response.status(201).send(faq);
+    }
+  );
 }
